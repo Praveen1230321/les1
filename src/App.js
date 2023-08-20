f92423f991168e87deb1f76f3bfd97cb270926bc
Praveen1230321 <dark512021@gmail.com>
2023-08-20 15:20:26 +0530

    fff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Greed from './components/Greed';
+import Welcome from './components/Welcome';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-     <Greed />
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div className='App'>
+        <Greed/>
+        <Welcome/>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Greed.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from '@testing-library/react';
+
+// function Greed(){
+    
+//     return<h1>Hello </h1>
+    
+// }
+const Greed = () => <h1>Hello</h1>
+
+export default Greed
